f8ecd3972d76798e85b6d679ff719b7ea3c90a36
Théophile DESRÉ <theophile.desre@student-cs.fr>
2024-11-15 23:46:31 +0100

    Intégration de l'exemple Cerise

--- site_web/description/views.py
+++ site_web/description/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
-
+from django.forms.models import model_to_dict
 from catalogue.models import Species
 
 def description(request, id):
-    if id > Species.objects.count():
+    if id > Species.objects.count() or id < 1 :
         return HttpResponse("URL incorrecte : il n'existe pas d'ID d'espèce n°"+str(id)+".")
-                            
     species = get_object_or_404(Species, id=id)
-    #return HttpResponse(species.name)
-    return render(request, 'description/description.html', {"id":id})
+    return render(request, 'description/description.html', model_to_dict(species))
 
 def error(request, text):
     return HttpResponse("URL incorrecte : entrer un nombre entre 1 et "+str(Species.objects.count()))

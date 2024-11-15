af98d9cee785b80867f977b3bc0905a5672f8e70
Philippine Froux <philippine.froux@student-cs.fr>
2024-11-15 16:10:18 +0100

    base de donnée : 10 nouveaux arbres

--- site_web/catalogue/models.py
+++ site_web/catalogue/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from catalogue import fruit_photos
+
 
 from django.db import models
 
 class Species(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    name_leaf = models.CharField(max_length=200, unique=True, blank=True)
-    name_fruit = models.CharField(max_length=200, unique=True, blank=True)
-    file_leaf = models.CharField(max_length=200, unique=True, blank=True)
-    file_fruit = models.CharField(max_length=200, unique=True, blank=True)
-    description = models.CharField(max_length=100000, unique=True, blank=True)
-    folder_gallery = models.CharField(max_length=200, unique=True, blank=True)
+    name_leaf = models.CharField(max_length=200, unique=False, blank=True)
+    name_fruit = models.CharField(max_length=200, unique=False, blank=True)
+    file_leaf = models.CharField(max_length=200, unique=False, blank=True)
+    file_fruit = models.CharField(max_length=200, unique=False, blank=True)
+    description = models.CharField(max_length=100000, unique=False, blank=True)
+    folder_gallery = models.CharField(max_length=200, unique=False, blank=True)
     keywords = models.CharField(max_length=1000, unique=False, blank=True)

d4e0f37070d3665308246cea34e12cf977bd527d
Théophile DESRÉ <theophile.desre@student-cs.fr>
2024-11-15 10:59:07 +0100

    Ajout de la classe Species

--- site_web/catalogue/models.py
+++ site_web/catalogue/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Species(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    name_leaf = models.CharField(max_length=200, unique=True, blank=True)
+    name_fruit = models.CharField(max_length=200, unique=True, blank=True)
+    file_leaf = models.CharField(max_length=200, unique=True, blank=True)
+    file_fruit = models.CharField(max_length=200, unique=True, blank=True)
+    description = models.CharField(max_length=100000, unique=True, blank=True)
+    folder_gallery = models.CharField(max_length=200, unique=True, blank=True)
+    keywords = models.CharField(max_length=1000, unique=False, blank=True)
--- site_web/site_web/settings.py
+++ site_web/site_web/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'accueil.apps.AccueilConfig',
+    'catalogue.apps.CatalogueConfig',
+    'description.apps.DescriptionConfig',
 ]
 
 

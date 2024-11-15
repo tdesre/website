0d005c8a8c21d33eeb41a7f7fccd5b066dfd0bef
Théophile DESRÉ <theophile.desre@student-cs.fr>
2024-11-15 16:16:53 +0100

    Ajout de la page d'arrivée de description

--- site_web/description/urls.py
+++ site_web/description/urls.py
@@ -1,1 +1,14 @@
-urlpatterns = []
+from django.conf import settings
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
+
+from . import views
+from . import models
+
+urlpatterns = [
+    path('<int:id>/', views.description, name="description"), # URL de chaque espèce : /description/[id espèce]
+    path('<str:text>/', views.error, name="error") # si l'URL /description/[texte] correspond à rien
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- site_web/description/views.py
+++ site_web/description/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+from catalogue.models import Species
+
+def description(request, id):
+    if id > Species.objects.count():
+        return HttpResponse("URL incorrecte : il n'existe pas d'ID d'espèce n°"+str(id)+".")
+                            
+    species = get_object_or_404(Species, id=id)
+    #return HttpResponse(species.name)
+    return render(request, 'description/description.html', {"id":id})
+
+def error(request, text):
+    return HttpResponse("URL incorrecte : entrer un nombre entre 1 et "+str(Species.objects.count()))

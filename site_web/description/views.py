1a215a7d6e83e3857fa8aa6997b6da0c7bf153f0
Théophile DESRÉ <theophile.desre@student-cs.fr>
2024-11-16 16:42:48 +0100

    Nombre automatique de photos

--- site_web/description/views.py
+++ site_web/description/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django.forms.models import model_to_dict
 from catalogue.models import Species
 
 def description(request, id):
-    if id > Species.objects.count() or id < 1 :
-        return HttpResponse("URL incorrecte : il n'existe pas d'ID d'espèce n°"+str(id)+".")
+    if id == 0 :
+        return redirect('/description/1')
+    if id > Species.objects.count():
+        return redirect('/description/'+str(Species.objects.count()))
+    
     species = get_object_or_404(Species, id=id)
-    return render(request, 'description/description.html', model_to_dict(species))
+    images = [{'full': f'description/images/fulls/{i:02}.jpg', 'thumb': f'description/images/thumbs/{i:02}.jpg', 'title': f'Title {i}', 'desc': f'Description for image {i}'} for i in range(1, 5)]
+    context= {**model_to_dict(species), 'images': images}
+
+    return render(request, 'description/description.html', context)
 
 def error(request, text):
     return HttpResponse("URL incorrecte : entrer un nombre entre 1 et "+str(Species.objects.count()))

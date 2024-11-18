ae0c9431195e8edfb8417468b315415d83714184
Tom Connery <tom.connery@student-cs.fr>
2024-11-18 10:48:50 +0100

    urls avec boutons

--- site_web/site_web/settings.py
+++ site_web/site_web/settings.py
@@ -124,9 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+'''
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / "site_web/static",
 ]
+'''
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 

6ebbd58eb10508d53d9f5b7b3f5e0b41bec098f0
Hortense <hortense.claudon@student-cs.fr>
2024-11-18 10:35:43 +0100

    import_template

--- site_web/catalogue/views.py
+++ site_web/catalogue/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Species
 
-# Create your views here.
+'''
+def index(request):
+    # Chargement du template avec le chemin relatif approprié
+    template = loader.get_template('catalogue/catalogue.html')
+    
+    # Rendu du template avec le contexte de la requête
+    return HttpResponse(template.render(request=request))
+'''
+
+def catalogue_feuilles(request):
+    feuilles = Species.objects.exclude(name_leaf="").all()
+    context = {'species_list': feuilles, 'type': 'Feuilles'}
+    return render(request, 'catalogue/catalogue.html', context)

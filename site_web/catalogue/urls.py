8560b2b181471e8a7ccde36c44fdc3bfee0c8703
Tom Connery <tom.connery@student-cs.fr>
2024-11-19 09:23:49 +0100

    recherche+resultat cliquable

--- site_web/catalogue/urls.py
+++ site_web/catalogue/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    # URL avec un paramètre dynamique 'text'
+    path('', views.catalogue_home, name='catalogue_home'),
     path('feuilles/', views.catalogue_feuilles, name='catalogue_feuilles'),
+    path('feuilles/<str:species_name>/', views.species_detail, name='species_detail'),  # Ajout de cette ligne
     path('search/<str:text>/', views.species_search_view, name='species_search'),
-    path('', views.catalogue_home, name='catalogue_home'),  # Page Catalogue
 ]
--- site_web/catalogue/views.py
+++ site_web/catalogue/views.py
@@ -3,12 +3,13 @@ from django.db.models import Q
 from .models import Species
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404
 
 def catalogue_feuilles(request):
     feuilles = Species.objects.exclude(name_leaf="").all()
     context = {'species_list': feuilles, 'type': 'Feuilles'}
     return render(request, 'catalogue/catalogue.html', context)
-
+'''
 def search_species(keyword):
     # Utilisation de Q pour construire la requête de recherche
     query = Q(keywords__icontains=keyword) | Q(name_leaf__icontains=keyword) | Q(name_fruit__icontains=keyword)
@@ -18,14 +19,22 @@ def search_species(keyword):
     
     # Retourner les résultats (par exemple, les noms des espèces trouvées)
     return results.values_list('name', flat=True)  # Renvoie une liste de noms d'espèces
-
+'''
+def search_species(keyword):
+    query = Q(keywords__icontains=keyword) | Q(name_leaf__icontains=keyword) | Q(name_fruit__icontains=keyword)
+    results = Species.objects.filter(query)  # Renvoie des objets Species
+    return results
 
 def species_search_view(request, text):
     keyword = text
     results = search_species(keyword)
-    print ("abs")
+    print("Résultats trouvés :", results)  # Vérifiez les données retournées
     return render(request, 'catalogue/search_results.html', {'results': results, 'keyword': keyword})
 
 
 def catalogue_home(request):
     return render(request, 'catalogue/catalogue_home.html')
+
+def species_detail(request, species_name):
+    species = get_object_or_404(Species, name=species_name)
+    return render(request, 'catalogue/species_detail.html', {'species': species})

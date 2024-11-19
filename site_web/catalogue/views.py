ad9d687af9bde7b2ba4ae4fb3867a1304dec15ea
Tarabula Ilan <ilan.tarabula@gmail.com>
2024-11-19 15:16:01 +0100

    section funfact + names

--- site_web/catalogue/views.py
+++ site_web/catalogue/views.py
@@ -8,10 +8,7 @@ from django.shortcuts import get_object_or_404
 def catalogue_feuilles(request):
     feuilles = Species.objects.exclude(file_leaf="")
     images = [{'type': 'Feuilles', 'path': feuille.file_leaf, 'id': feuille.id, 'portfolio_id':'portfolioModal'+str(feuille.id)} for feuille in feuilles]
-    dico = {}
-    for i in range(len(images)):
-        dico[f'{i}'] = (images[i], feuilles[i])
-    return render(request, 'catalogue/catalogue_feuilles.html', {'dico' : dico})
+    return render(request, 'catalogue/catalogue_feuilles.html', {'images' : images})
     
 
 def catalogue_fruits(request):
@@ -39,18 +36,18 @@ def species_detail(request, species_name):
     return render(request, 'catalogue/species_detail.html', {'species': species})
 
 
-def rate_species(request, species_id):
-    species = get_object_or_404(Species, id=species_id)
-
-    if request.method == "POST":
-        score = int(request.POST.get('score'))
-        rating, created = Rating.objects.update_or_create(
-            user=request.user,
-            species=species,
-            defaults={'score': score}
-        )
-        # Afficher les informations dans la console
-        print(f"L'utilisateur {request.user.username} a noté {species.name} avec un score de {score}.")
-        return redirect('catalogue')  # Redirige vers la page du catalogue
-
-    return render(request, 'catalogue/rate_species.html', {'species': species})
+# def rate_species(request, species_id):
+#    species = get_object_or_404(Species, id=species_id)
+#
+#    if request.method == "POST":
+#        score = int(request.POST.get('score'))
+#        rating, created = Rating.objects.update_or_create(
+#            user=request.user,
+#            species=species,
+#            defaults={'score': score}
+#        )
+#        # Afficher les informations dans la console
+#        print(f"L'utilisateur {request.user.username} a noté {species.name} avec un score de {score}.")
+#        return redirect('catalogue')  # Redirige vers la page du catalogue
+#
+#    return render(request, 'catalogue/rate_species.html', {'species': species})

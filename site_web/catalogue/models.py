65466b70bc2a04ef7e8201060636d68086129c8a
Philippine Froux <philippine.froux@student-cs.fr>
2024-11-20 09:52:24 +0100

    migrations

--- site_web/catalogue/models.py
+++ site_web/catalogue/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User  # Utilisé pour associer les notes aux utilisateurs
+from catalogue import fruit_photos
+from django import forms
 
 class Species(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    name_leaf = models.CharField(max_length=200, blank=True)
-    #j'ai retire unique = True car ca ne me permettait pas de
-    # migrate
-    name_fruit = models.CharField(max_length=200, blank=True)
+    name_leaf = models.CharField(max_length=200, unique=False, blank=True)
+    name_fruit = models.CharField(max_length=200, unique=True, blank=True)
     file_leaf = models.CharField(max_length=200, unique=True, blank=True)
     file_fruit = models.CharField(max_length=200, unique=True, blank=True)
     description = models.CharField(max_length=100000, unique=True, blank=True)
     folder_gallery = models.CharField(max_length=200, unique=True, blank=True)
     keywords = models.CharField(max_length=1000, unique=False, blank=True)
-    
+    user_name= models.CharField(max_length=200, unique=False, blank=True)
+    loc = models.CharField(max_length=200, unique=False, blank=True)
 
-    def __str__(self):
-        return self.name
+def __str__(self):
+    return self.name

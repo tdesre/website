1986907eb0cc2cadb8dce420ec7a56cabd9f5bc4
Théophile DESRÉ <theophile.desre@student-cs.fr>
2024-11-20 19:03:53 +0100

    Créer son compte utilisateur

--- site_web/accueil/views.py
+++ site_web/accueil/views.py
@@ -22,3 +22,23 @@ def Connect(request):
             return HttpResponse("error")
         
     return redirect('accueil')     #Si ça ne fonctionne pas
+
+@csrf_exempt
+def Register(request):
+    if request.method == 'POST':
+        # Récupérer les données envoyées avec le formulaire
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print("1")
+        if User.objects.filter(username=username).count() ==1:
+            print("2")
+            return HttpResponse("Username déjà pris")
+        else:
+            print("3")
+            try : 
+                user = User.objects.create_user(username=username, password=password)
+                return HttpResponse("success")
+            except Exception as e:
+                return HttpResponse("error"+str(e))
+    print("4")
+    return redirect('accueil')     #Si ça ne fonctionne pas
